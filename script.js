b5fb3e9bb1f4b1ccadb0922ac52346cc1646c4f7
Oluwatosin <146461006+Ultimate-Guru@users.noreply.github.com>
2024-05-12 18:43:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("togglePassword").addEventListener("change", function () {
+    var passwordInput = document.getElementById("password");
+    if (this.checked) {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password"
+    }
+});
